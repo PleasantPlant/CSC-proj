c67db0ff4c2473cd21428252a810dd5b6e64e406
PleasantPlant <yoshihiro.m.shimokawa@gmail.com>
2022-11-13 16:25:59 -0500

    updating project to meet assignment reqs

---
+++ Assets/Scenes/Driver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+
+    [SerializeField] float steerSpeed = 0.1f;
+
+    [SerializeField] float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Rotate(0,0,steerAmount * steerSpeed);
+        transform.Translate(0,moveAmount * moveSpeed,0);
+    }
+}
